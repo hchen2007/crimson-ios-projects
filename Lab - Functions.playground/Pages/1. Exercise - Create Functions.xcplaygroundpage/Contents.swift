a88c74f52f896e2ff7f40062254eae60f3c34463
hchen2007 <86478204+hchen2007@users.noreply.github.com>
2021-12-19 07:20:43 +0700

    Update Contents.swift

--- Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -4,10 +4,34 @@
  Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
  */
 
+func introduceMyself() {
+     print("I'm Hung Yu. Nice to meet you.")
+}
 
 /*:
  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
  */
 
+import Foundation
+
+func magicEightBall() {
+    let randomNum = Int.random(in: 0...4)
+    switch randomNum {
+    case 0:
+        print("This is a 0.")
+    case 1:
+        print("This is a 1.")
+    case 2:
+        print("This is a 2.")
+    case 3:
+        print("This is a 3.")
+    case 4:
+        print("This is a 4.")
+    default:
+        print("I don't know that number")
+    }
+}
+
+magicEightBall()
 
 //: page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
